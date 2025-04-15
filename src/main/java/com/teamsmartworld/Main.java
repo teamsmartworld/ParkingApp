a6b47a206989742c1af29613fc807fbdd03e06e1
DaveTeamSmartWorld <teamsmartworld@hotmail.com>
2025-04-15 22:07:02 +0200

    Added parking spot and its main method

--- src/main/java/com/teamsmartworld/Main.java
+++ src/main/java/com/teamsmartworld/Main.java
@@ -3,6 +3,7 @@ package com.teamsmartworld;
 
 
 import com.teamsmartworld.model.Customer;
+import com.teamsmartworld.model.ParkingSpot;
 
 public class Main {
 
@@ -10,5 +11,11 @@ public class Main {
         Customer customer = new Customer(1, "CustomerName1", "0123456789", "ABC-123");
         customer.setVehiclePlateNumber("121212121212");
         System.out.println("customer = " + customer);
+
+        ParkingSpot spot1 = new ParkingSpot (1, 101);
+        spot1.occupy();
+        System.out.println("spot1 = " + spot1);
+        spot1.vacate();
+        System.out.println("spot1 = " + spot1);
     }
 }
--- src/main/java/com/teamsmartworld/model/ParkingSpot.java
+++ src/main/java/com/teamsmartworld/model/ParkingSpot.java
@@ -1,4 +1,57 @@
 package com.teamsmartworld.model;
 
 public class ParkingSpot {
+
+    //Fields
+    private Integer spotNumber;
+    private Integer areaCode;
+    private  boolean occupied;
+
+
+    //Constructors
+
+    public ParkingSpot(Integer spotNumber, Integer areaCode) {
+        this.spotNumber = spotNumber;
+        this.areaCode = areaCode;
+        this.occupied = true;
+    }
+
+    //Methods, Getters and Setters
+
+    public Integer getSpotNumber() {
+        // todo: add validation
+        return spotNumber;
+    }
+
+    public void setSpotNumber(Integer spotNumber) {
+        this.spotNumber = spotNumber;
+    }
+
+    public Integer getAreaCode() {
+        return areaCode;
+    }
+
+    public void setAreaCode(Integer areaCode) {
+        this.areaCode = areaCode;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+
+    public void occupy() {
+        this.occupied = true;
+    }
+    public void vacate() {
+        this.occupied = false;
+    }
+
+    @Override
+    public String toString() {
+        return "ParkingSpot{" +
+                "spotNumber=" + spotNumber +
+                ", areaCode=" + areaCode +
+                ", occupied=" + occupied +
+                '}';
+    }
 }
