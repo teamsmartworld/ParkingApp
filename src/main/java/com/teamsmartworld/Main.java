01e85fbf6acabbbe5631f591745eb9adeb160ed2
DaveTeamSmartWorld <teamsmartworld@hotmail.com>
2025-04-15 23:24:11 +0200

    Added Reservation class and its main method

--- src/main/java/com/teamsmartworld/Main.java
+++ src/main/java/com/teamsmartworld/Main.java
@@ -4,18 +4,16 @@ package com.teamsmartworld;
 
 import com.teamsmartworld.model.Customer;
 import com.teamsmartworld.model.ParkingSpot;
+import com.teamsmartworld.model.Reservation;
 
 public class Main {
 
     public static void main(String[] args) {
         Customer customer = new Customer(1, "CustomerName1", "0123456789", "ABC-123");
-        customer.setVehiclePlateNumber("121212121212");
-        System.out.println("customer = " + customer);
-
         ParkingSpot spot1 = new ParkingSpot (1, 101);
-        spot1.occupy();
         System.out.println("spot1 = " + spot1);
-        spot1.vacate();
-        System.out.println("spot1 = " + spot1);
+
+        Reservation reservation1 = new Reservation(customer, spot1, 2);
+        System.out.println(reservation1);
     }
 }
--- src/main/java/com/teamsmartworld/model/Reservation.java
+++ src/main/java/com/teamsmartworld/model/Reservation.java
@@ -1,4 +1,80 @@
 package com.teamsmartworld.model;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class Reservation {
+    private String reservationId;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Customer customer;
+    private ParkingSpot parkingSpot;
+    private Status status;
+
+    // Constructor
+
+    public Reservation(Customer customer, ParkingSpot parkingSpot, int hours) {
+        this.reservationId = UUID.randomUUID().toString();
+        this.startTime = LocalDateTime.now();
+        this.endTime = startTime.plusHours(hours);
+        this.customer = customer;
+        this.parkingSpot = parkingSpot;
+        this.status = Status.ACTIVE;
+    }
+
+//methods (Getter, Setters and other methods)
+
+    public String getReservationId() {
+        return reservationId;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public ParkingSpot getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setEndTime(int hours){
+        if (hours <= 0) throw new IllegalArgumentException("Hours should be a positive number");
+        this.endTime = this.endTime.plusHours(hours);
+    }
+
+    public void complete (){
+        this.status = Status.COMPLETED;
+    }
+
+//override
+
+    @Override
+    public String toString() {
+        return """
+                Reservation Details
+                Reservation ID = %s
+                Start Time = %s
+                End Time = %s
+                Parking Spot = %s
+                Customer = %s
+                """ .formatted(
+                        reservationId,
+                        startTime,
+                        endTime,
+                        parkingSpot,
+                        customer);
+    }
 }
+
+
